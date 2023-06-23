11d6771bdd56564b8d343daf535de09ceb990223
CKonkol <CKonkol@ippathways.com>
2023-06-23 14:06:40 -0500

    Some quick updates to notes management

---
+++ custom_tools/notes_management/TODO.md
@@ -0,0 +1,9 @@
+* Refine Ideas subcommand
+* Build out notes project help
+* Build out notes idea help
+* Make notes project -l perdy
+* Create notes list command that shows projects and ideas
+* Add way to configure repository for projects
+* Create configuration file or settings in order to allow specifying notes location
+* Multiple workspaces?
+
--- custom_tools/notes_management/lib/api/project.sh
+++ custom_tools/notes_management/lib/api/project.sh
@@ -2,12 +2,62 @@
 
 # called to update notes on a specific project
 # Usage: notes project MYAC-123 opens editor for notes on that project
+# Arguments: 
+# -l --list list all projects
+# -h --help help with command
 
-function notes_project() {
+
+
+function notes_project_help() {
+	echo "Notes Project help TODO"
+	exit 0
+}
+function notes_project_list() {
+	ls -I "Ideas" "$_NOTES_FOLDER"
+	exit 0
+}
+function notes_project_all() {
+	exit 0
+	
+}
+function notes_project() {	
+
+	# parse options
+	
+	VALID_PROJECT_ARGS=$(getopt -o hla --long help,list,all -- "$@")
+	if [[ $? -ne 0 ]]; then
+		exit 1;
+	fi
+
+	eval set -- "$VALID_PROJECT_ARGS"
+	while [ : ]; do
+		case "$1" in
+			-h | --help)
+				shift
+				notes_project_help $@
+				break
+				;;
+			-l | --list)
+				shift
+				notes_project_list $@
+				break
+				;;
+			-a | --all)
+				shift
+				notes_project_all $@
+				break
+				;;
+			--) shift;
+				break
+				;;
+		esac
+	done
+			
 	local project_name=${1}
 	
 	mkdir -p "$_NOTES_FOLDER/$project_name" # init directory for project if not exists
-	hx "$_NOTES_FOLDER/$project_name/notes.md"
+	editor "$_NOTES_FOLDER/$project_name/notes.md"
 
 	return 0
 }
+
